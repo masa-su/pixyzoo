fix Nan problem in KL

DMM/dmm_without_pixyz.py
@@ -69,14 +69,13 @@ train_loader, test_loader, t_max = init_dataset(batch_size)
 
 # In[6]:
 # Loss in https://github.com/clinicalml/dmm/blob/master/model_th/dmm.py
-def KLGaussianGaussian(phi_mu, phi_sigma, prior_mu, prior_sigma, eps=1e-5):
+def KLGaussianGaussian(phi_mu, phi_sigma, prior_mu, prior_sigma, eps=0.0001):
     '''
     Re-parameterized formula for KL
     between Gaussian predicted by encoder and Gaussian dist
     eps: small number added to variances to avoid NaNs
     '''
-    phi_sigma += eps
-    prior_sigma += eps
+    prior_sigma = prior_sigma + eps
     kl = 0.5 * (2 * torch.log(prior_sigma) - 2 * torch.log(phi_sigma) + (phi_sigma.pow(2) + (phi_mu - prior_mu).pow(2)) / prior_sigma.pow(2) - 1)
     kl = torch.sum(kl, dim=1).mean()
     return kl